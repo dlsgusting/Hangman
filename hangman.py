19f5931e3eee264ec1391977d16740eb22660c55
Jason <hnhjknmn@gmail.com>
2026-06-20 05:27:59 -0400

    Changed game function, moved stuff outside of it

--- hangman.py
+++ hangman.py
@@ -2,15 +2,27 @@ import random
 playing = True
 
 words = ["hello","bye", "today", "what", "goodbye"]
+valid_answer = "abcdefghijklmnopqrstuvwxyz"
 used_letters = ["a","b","c"]
 
-def game():
-    random_int = random.randint(0, len(words) -1)
-    guess_list = []
-    for i in range(len(words[random_int])):
-        guess_list.append(0)
-    print(f"Current solve: {guess_list}")
+random_int = random.randint(0, len(words) -1)
+guess_list = {}
+guess_list_display = []
+for i in words[random_int]:
+    guess_list[i] = 0
+    guess_list_display.append(0)
+
+def choose_letter():
+    print("Choose a letter a-z")
+    letter = input()
+    while letter not in valid_answer:
+        print("You didn't choose a letter, choose gain.")
+        letter = input()
+    return letter
+
+
 
 while playing:
+    print(f"Current solve: {guess_list_display}")
     game()
     playing = False
